6422165ab1d25137e016c86ac053e85875802174
Fadri2610 <fabiflow102004@gmail.com>
2025-03-30 01:38:23 -0500

    Update

--- bliss.sh
+++ bliss.sh
@@ -33,15 +33,14 @@ fi
 echo "🔄 Syncing sources..."
 /opt/crave/resync.sh
 
+# Clean
+#echo "🧹 Running installclean..."
+#make installclean
+
 # Set up build environment
 echo "🔧 Setting up build environment..."
 source build/envsetup.sh
 
 # Build configuration
 lunch bliss_vayu-user
-
-# Clean
-echo "🧹 Running installclean..."
-make installclean
-
-blissify -g -d vayu
+blissify -g vayu
