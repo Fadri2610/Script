59a666ecf72343077318b01218160eda7cf395f4
bagaskara815 <bagaskara815@gmail.com>
2025-04-07 00:04:40 -0500

    initial script

---
+++ orion.sh
@@ -0,0 +1,45 @@
+echo "🗑️ Removing old parts directories..."
+rm -rf .repo/local_manifests/
+rm -rf device/xiaomi
+rm -rf kernel/xiaomi
+rm -rf vendor/xiaomi
+
+# Clone ROM source
+echo "📦 Cloning ROM source..."
+repo init -u https://github.com/OrionOS-Project/manifest -b vic --git-lfs
+
+# Clone local_manifests repository
+echo "Cloning local_manifests..."
+git clone --depth=1 https://github.com/Fadri2610/bagaskara_local_manifests.git -b orion .repo/local_manifests
+echo "✅ local_manifests cloned successfully."
+
+# repo sync
+echo "🔄 Syncing sources..."
+/opt/crave/resync.sh
+
+# Set up build environment
+echo "🔧 Setting up build environment..."
+source build/envsetup.sh
+
+export ORION_MAINTAINER=Fabi
+export ORION_MAINTAINER_LINK=https://t.me/fabianflores2610
+export ORION_BUILD_TYPE=Unofficial
+export ORION_GAPPS=true
+export TARGET_BOOT_ANIMATION_RES=1080
+
+# Clean
+echo "🧹 Running clean..."
+make deviceclean
+
+# Build configuration
+echo "🛠️ Start build configuration..."
+lunch orion_vayu-ap4a-userdebug
+
+echo -e "\n
+   ╔════════════════════════╗
+   ║     OrionOS Project    ║
+   ╚════════════════════════╝
+\n"
+
+echo "🚀 Start compiling..."
+make orion -j$(nproc --all)
