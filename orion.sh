be9e967085cde266aa9f1b19a4b83c987ba96481
bagaskara815 <bagaskara815@gmail.com>
2025-04-08 21:01:15 -0500

    upd

--- orion.sh
+++ orion.sh
@@ -27,8 +27,8 @@ export ORION_GAPPS=true
 export TARGET_BOOT_ANIMATION_RES=1080
 
 # Clean
-#echo "🧹 Running clean..."
-#make deviceclean
+echo "🧹 Running clean..."
+make deviceclean
 
 # Build configuration
 echo "🛠️ Start build configuration..."
