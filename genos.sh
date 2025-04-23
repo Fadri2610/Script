91f14a706447b7ff5bd1203e1a248b2b87a670c6
Fadri2610 <fabiflow102004@gmail.com>
2025-04-22 20:29:02 -0500

    Genify!

--- genos.sh
+++ genos.sh
@@ -3,32 +3,34 @@ rm -rf .repo/local_manifests/
 rm -rf device/xiaomi
 rm -rf kernel/xiaomi
 rm -rf vendor/xiaomi
+
 # Clone ROM source
 echo "📦 Cloning ROM source..."
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-15.1 --git-lfs
+repo init -u https://github.com/GenesisOS/manifest.git -b verve-qpr2 --git-lfs
 
 # Clone repos
 echo "🔹 Cloning repositories..."
-git clone --depth=1 -b adroid https://github.com/Fadri2610/bagaskara_local_manifests.git .repo/local_manifests
+git clone --depth=1 -b genos https://github.com/Fadri2610/build_roomservice.git .repo/local_manifests
 echo "✅ Cloning completed!"
 
 # repo sync
 echo "🔄 Syncing sources..."
 /opt/crave/resync.sh
 
+# Build flags
+export GENESIS_MAINTAINER=Fabi 
+
 # Set up build environment
 echo "🔧 Setting up build environment..."
 . build/envsetup.sh
 
-# Build configuration (Set build type)
+# Build configuration
 echo "🛠️ Start build configuration..."
-# lunch alpha_$device_codename-eng    # For bring-up
-lunch alpha_vayu-userdebug  # For testing  
-# lunch alpha_$device_codename-user       # For release
+breakfast vayu
 
 # Clean
 echo "🧹 Running installclean..."
 make deviceclean
-# Start build 
+
 echo "🚀 Start compiling..."
-make bacon
+mka genesis
